a6a4acb39de4f29107ffb1734e85691b6aee4dc6
Oliver-Tobias Ripka <contact@oripka.de>
2023-03-25 10:32:58 +0100

    More features and readme

--- README.md
+++ README.md
@@ -1,71 +1,55 @@
-# chatgpt-process-directory README
+Here's a sample `README.md` for the ChatGPT File Processor extension:
 
-This is the README for your extension "chatgpt-process-directory". After writing up a brief description, we recommend including the following sections.
+```markdown
+# ChatGPT File Processor
+
+ChatGPT File Processor is a Visual Studio Code extension that allows you to process a directory of files (and its subdirectories) using OpenAI's ChatGPT API.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- Iterate over a folder and its subfolders to process files of a specific type (e.g., `*.md`).
+- Configure the system message used for processing each file.
+- The output for each file is written to a new directory structure with the suffix `_processed`.
+- Configure the API key and model via the extension configuration.
+- Test run functionality to process just the first two files.
+- Library of system prompts that can be kept in the configuration and be selectable.
+- Option to use a custom system prompt for each run.
+- Command to add a new system prompt to the library.
 
-For example if there is an image subfolder under your extension project workspace:
+## Getting Started
 
-\!\[feature X\]\(images/feature-x.png\)
+1. Install the extension in Visual Studio Code.
+2. Configure the API key, model, and file type in the extension settings.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+## Usage
 
-## Requirements
+1. Open the Command Palette (`Ctrl+Shift+P` or `Cmd+Shift+P` on Mac) and type "Process Files with ChatGPT", then select the command from the list.
+2. Select the input directory containing the files you want to process.
+3. Select the output directory where the processed files will be saved. The output files will be saved in a new directory structure with the suffix `_processed`.
+4. Choose a system prompt from the library or enter a custom system prompt.
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+## Configuration
 
-## Extension Settings
+You can configure the extension settings through the Visual Studio Code settings:
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+- `chatgpt-file-processor.apiKey`: The API key for ChatGPT.
+- `chatgpt-file-processor.model`: The model to use for ChatGPT (e.g., "gpt-3.5-turbo").
+- `chatgpt-file-processor.fileType`: The file type to process (e.g., "*.md", "*.txt").
+- `chatgpt-file-processor.testRun`: Test run option (process only the first 2 files).
+- `chatgpt-file-processor.systemPrompts`: Library of system prompts.
 
-For example:
+## Commands
 
-This extension contributes the following settings:
+- `Process Files with ChatGPT`: Process a directory of files with ChatGPT.
+- `Add System Prompt to Library`: Add a new system prompt to the library.
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+## Contributing
 
-## Known Issues
+If you want to contribute to this project or report a bug, please open an issue or submit a pull request on the [GitHub repository](https://github.com/oripka/chatgpt-file-processor).
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+## License
 
-## Release Notes
+This extension is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+```
 
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Replace the sample GitHub repository URL with the actual URL of your repository. You can customize the `README.md` further according to your requirements. Add this file to your extension's root directory, and it will be displayed as the extension's description in the Visual Studio Code marketplace.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,258 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import { Configuration, OpenAIApi } from "openai";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+async function shouldOverwriteFile(
+  filePath: string,
+  alwaysOverwrite: boolean,
+  neverOverwrite: boolean
+): Promise<boolean> {
+  if (alwaysOverwrite) {
+    return true;
+  }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatgpt-process-directory" is now active!');
+  if (neverOverwrite) {
+    return false;
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgpt-process-directory.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from chatgpt-process-directory!');
-	});
+  const overwriteOptions = ["Yes", "No", "Always", "Never"];
+  const result = await vscode.window.showInformationMessage(
+    `File ${filePath} already exists. Overwrite?`,
+    ...overwriteOptions
+  );
 
-	context.subscriptions.push(disposable);
+  switch (result) {
+    case "Yes":
+      return true;
+    case "No":
+      return false;
+    case "Always":
+      alwaysOverwrite = true;
+      return true;
+    case "Never":
+      neverOverwrite = true;
+      return false;
+    default:
+      return false;
+  }
 }
 
-// This method is called when your extension is deactivated
+async function addSystemPrompt(prompt: string) {
+  const systemPrompts = vscode.workspace
+    .getConfiguration("chatgpt-file-processor")
+    .get("systemPrompts") as string[];
+  systemPrompts.push(prompt);
+  await vscode.workspace
+    .getConfiguration("chatgpt-file-processor")
+    .update("systemPrompts", systemPrompts, vscode.ConfigurationTarget.Global);
+}
+
+async function processFile(
+  filePath: string,
+  systemMessage: string,
+  apiKey: string,
+  model: string
+): Promise<string> {
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+
+  const configuration = new Configuration({
+    apiKey: apiKey,
+  });
+  const openaiApi = new OpenAIApi(configuration);
+
+  console.log("Processing: " + filePath);
+  const completion = await openaiApi.createChatCompletion({
+    model: model,
+    messages: [
+      { role: "system", content: systemMessage },
+      { role: "user", content: fileContent },
+    ],
+  });
+
+  console.log("Output: " + completion);
+  if (!completion?.data?.choices[0]?.message?.content) {
+    throw new Error(`Empty response for file: ${filePath}`);
+  }
+
+  return completion.data.choices[0].message.content;
+}
+
+async function processFiles(
+  inputDir: string,
+  outputDir: string,
+  fileType: string,
+  systemMessage: string,
+  apiKey: string,
+  model: string,
+  testRun: boolean
+) {
+  let filesProcessed = 0;
+  let alwaysOverwrite = false;
+  let neverOverwrite = false;
+
+  async function processDirectory(inputPath: string, outputPath: string) {
+    const entries = fs.readdirSync(inputPath, { withFileTypes: true });
+
+    for (const entry of entries) {
+      if (entry.isDirectory()) {
+        await processDirectory(
+          path.join(inputPath, entry.name),
+          path.join(outputPath, entry.name)
+        );
+      } else if (entry.isFile() && entry.name.endsWith(fileType)) {
+        const inputFilePath = path.join(inputPath, entry.name);
+        const outputFilePath = path.join(outputPath, entry.name);
+
+        if (fs.existsSync(outputFilePath)) {
+          const overwrite = await shouldOverwriteFile(
+            outputFilePath,
+            alwaysOverwrite,
+            neverOverwrite
+          );
+
+          if (!overwrite) {
+            continue;
+          }
+
+          if (alwaysOverwrite) {
+            neverOverwrite = false;
+          }
+
+          if (neverOverwrite) {
+            alwaysOverwrite = false;
+          }
+        }
+
+        try {
+          const processedContent = await processFile(
+            inputFilePath,
+            systemMessage,
+            apiKey,
+            model
+          );
+
+          fs.mkdirSync(path.dirname(outputFilePath), { recursive: true });
+          fs.writeFileSync(outputFilePath, processedContent);
+
+          filesProcessed++;
+          if (testRun && filesProcessed >= 2) {
+            break;
+          }
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            vscode.window.showErrorMessage(
+              `Error processing file ${inputFilePath}: ${error.message}`
+            );
+          } else {
+            vscode.window.showErrorMessage(
+              `Error processing file ${inputFilePath}: ${String(error)}`
+            );
+          }
+        }
+      }
+    }
+  }
+
+  await processDirectory(inputDir, outputDir);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "chatgpt-file-processor.processFiles",
+    async () => {
+      const inputDir = await vscode.window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: "Select input directory",
+      });
+
+      if (!inputDir) {
+        return;
+      }
+
+      const outputDir = await vscode.window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: "Select output directory",
+      });
+
+      if (!outputDir) {
+        return;
+      }
+
+      const systemPrompts = vscode.workspace
+        .getConfiguration("chatgpt-file-processor")
+        .get("systemPrompts") as string[];
+
+      const selectedPrompt = await vscode.window.showQuickPick(
+        [...systemPrompts, "Custom"],
+        {
+          placeHolder:
+            "Select a system prompt from the library or choose Custom to enter a new one",
+        }
+      );
+
+      let systemMessage;
+      if (selectedPrompt === "Custom") {
+        systemMessage = await vscode.window.showInputBox({
+          prompt: "Enter the custom system prompt:",
+        });
+      } else {
+        systemMessage = selectedPrompt;
+      }
+
+      if (!systemMessage) {
+        return;
+      }
+
+      const fileType = vscode.workspace
+        .getConfiguration("chatgpt-file-processor")
+        .get("fileType") as string;
+      const apiKey = vscode.workspace
+        .getConfiguration("chatgpt-file-processor")
+        .get("apiKey") as string;
+      const model = vscode.workspace
+        .getConfiguration("chatgpt-file-processor")
+        .get("model") as string;
+      const testRun = vscode.workspace
+        .getConfiguration("chatgpt-file-processor")
+        .get("testRun") as boolean;
+
+      await processFiles(
+        inputDir[0].fsPath,
+        outputDir[0].fsPath + "_processed",
+        fileType,
+        systemMessage,
+        apiKey,
+        model,
+        testRun
+      );
+      vscode.window.showInformationMessage("Files processed successfully.");
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  let addPromptDisposable = vscode.commands.registerCommand(
+    "chatgpt-file-processor.addSystemPrompt",
+    async () => {
+      const newPrompt = await vscode.window.showInputBox({
+        prompt: "Enter the new system prompt:",
+      });
+
+      if (newPrompt) {
+        await addSystemPrompt(newPrompt);
+        vscode.window.showInformationMessage(
+          "System prompt added to the library."
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(addPromptDisposable);
+}
+
 export function deactivate() {}
